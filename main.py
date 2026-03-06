8f11835a66f53aeab33b937fd7234870d7d44e47
MishelleMacDoc <michaela.valach@proton.me>
2026-03-06 16:14:40 +0100

    Fix CORS issue

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",      
         "http://127.0.0.1:5173",       
-        "https://insaytia-app.web.app/",
+        "https://insaytia-app.web.app",
         "https://insaytia-app.firebaseapp.com"
     ],
     allow_credentials=True,
